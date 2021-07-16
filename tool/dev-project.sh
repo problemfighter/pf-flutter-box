9b9adcf6c8524980735a7eff996b98ce667e359e
hmtmcse <hmtm.cse@gmail.com>
2021-07-17 04:42:15 +0600

    bismillah

---
+++ tool/dev-project.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir box_dependencies
+cd box_dependencies
+
+git clone https://github.com/problemfighter/pf-flutter-core.git
+git clone https://github.com/problemfighter/pf-flutter-mui.git
+git clone https://github.com/problemfighter/pf-flutter-form.git
